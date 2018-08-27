685052cbc2d74cb9c230dfd4cd95408e226c6d05
yosu <woodstock830@gmail.com>
2018-08-28 00:56:13 +0900

    Add 26

---
+++ chapter03/26.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# 26. 強調マークアップの除去
+#
+# 25の処理時に，テンプレートの値からMediaWikiの強調マークアップ（弱い強調，強調，強い強調のすべて）を除去してテキストに変換せよ（参考: マークアップ早見表）．
+import sys
+import re
+
+def extract(text):
+    p = re.compile(u"(?P<content>.*?)(?P<quote>'{2,4})(?P<highlighted_content>.*?)(?P=quote)(?P<remaining_content>.*)")
+    m = p.match(text)
+    if m:
+        content = m.group('content')
+        highlighted_content = m.group('highlighted_content')
+        remaining_content = m.group('remaining_content')
+        return content + highlighted_content + extract(remaining_content)
+
+    return text
+
+for line in sys.stdin:
+    text = line.rstrip()
+    print(extract(text))
